97a068e6f0c8e249bf3ea6e6f4626958873e6a2e
Ploic <loic.pauletto@gmail.com>
2016-02-05 10:09:20 +0100

    First commit

---
+++ Exo1/src/Animal.java
@@ -0,0 +1,35 @@
+/**
+ * Created by paulettl on 05/02/16.
+ */
+public class Animal {
+
+    private String name;
+    private Integer weight;
+
+
+    public Animal(String name, Integer weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public void crier(){
+        System.out.println(name + "crie...");
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+}
